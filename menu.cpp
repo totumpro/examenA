defa931f75f0337c799a42629c62079f22e26df7
LoreenRH <lorena.rangel@docker.com>
2017-10-29 20:36:06 +0100

    Agregar menu básico de operaciones
    Signed-off-by: LoreenRH <lorena.rangel@docker.com>

---
+++ menu.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "suma.h"
+
+using namespace std;
+
+int main() {
+    int opt = 1;
+    switch(opt){
+        case 1:
+            /* Suma */
+            break;
+        case 2:
+            /* Resta */
+            break;
+        case 3:
+            /* Multiplicacion */
+            break;
+        case 4:
+            /* Division */
+            break;
+        case 5:
+            /* Salir */
+            break;
+        default:
+        printf("Opcion no valida");
+        
+    }   
+
+    return 0;
+}
